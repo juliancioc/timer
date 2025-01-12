d5909a085b2dcd531cd67721f82a834cba2cb913
Juliancio Carvalho <juliancio.carvalho.ext@dasa.com.br>
2025-01-12 16:48:17 -0300

    feat: routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,17 @@
+import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
+
+import { defaultTheme } from "./styles/themes/default";
+import { GlobalStyle } from "./styles/global";
+import { Router } from "./Router";
+
 export function App() {
-  return <h1>Timer</h1>;
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+    </ThemeProvider>
+  );
 }
