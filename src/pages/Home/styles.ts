3b183f7dbe92838a5c9af9f0d0ba0fc9ba250c1d
= <juliancio_alves@hotmail.com>
2025-01-18 14:57:00 -0300

    test

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -60,7 +60,7 @@ const BaseInput = styled.input`
   font-weight: bold;
   font-size: 1.125rem;
   padding: 0 0.5rem;
-  color: ${({ theme }) => theme["gray-500 "]};
+  color: ${({ theme }) => theme["gray-500"]};
 
   &:focus {
     box-shadow: none;
